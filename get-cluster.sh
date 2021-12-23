f15a669a19a0a472d5b612d530097538edb3ee2b
Your Name <you@example.com>
2021-12-23 15:22:12 +0000

    kubctl script

--- get-cluster.sh
+++ get-cluster.sh
@@ -3,7 +3,7 @@
 
 CLUSTERNAME=$(aws eks list-clusters)
 REGION='us-east-1'
-COMMAND=$(aws eks --region $REGION update-kubeconfig --name $CLUSTERNAME)
+COMMAND=$(aws eks --region $REGION update-kubeconfig --name eksworkshop-eksctl)
 
 echo $CLUSTERNAME
 echo -e $REGION
